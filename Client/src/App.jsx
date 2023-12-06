ec1616cadf4e8705d33da8027e5511549f57d686
PhurinGZ <120617446+PhurinGZ@users.noreply.github.com>
2023-12-06 16:28:05 +0700

    first commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,17 @@
+// this is router
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<HomePage />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
