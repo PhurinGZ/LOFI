4f525e9dad9618fbd7de6c2ae014f352d8d246b3
PhurinGZ <120617446+PhurinGZ@users.noreply.github.com>
2023-12-22 00:35:05 +0700

    fix  SpeedInsights  V_2

--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { SpeedInsights } from '@vercel/speed-insights/react';
+import { SpeedInsights } from '@vercel/speed-insights';
 import Demo from "../audioplayer/Demo";
 import "./Home.scss";
 import Header from "../../layout/header/head";
