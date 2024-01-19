49916fbe03b8803e5c3e1f0c9db120617f033b85
PhurinGZ <120617446+PhurinGZ@users.noreply.github.com>
2024-01-19 13:14:40 +0700

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
               <Route path="/category" element={<Cat />} />
               <Route path="/sidebar" element={<Sidebar />} />
               <Route
-                path="/verify-email"
+                path="/verify-email/:emailToken"
                 element={<VerifyEmail />}
               ></Route>
               {/* <Route path="/:auth" element={<Register />} />
