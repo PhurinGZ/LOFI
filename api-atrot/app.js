052121ee6032ac5d1a3140bf3aaff14a057f719e
Thujewa Pradaproikaeo <uht009@gmail.com>
2024-02-07 22:46:47 +0700

    api-tarot

---
+++ api-atrot/app.js
@@ -0,0 +1,148 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const path = require("path");
+const cloneDeep = require("lodash.clonedeep");
+const remove = require("lodash.remove");
+const cors = require("cors");  // เพิ่มบรรทัดนี้
+
+const app = express();
+const router = express.Router();
+
+const root =
+  process.env.NODE_ENV === "production"
+    ? path.join(__dirname, "..")
+    : __dirname;
+
+app.use(bodyParser.json());
+app.use("/static", express.static(path.join(root, "static")));
+
+app.get("/", (_req, res) => {
+  return res.sendFile("static/index.html", { root });
+});
+
+app.use("/api/v1/", router);
+
+router.get("/docs", (_req, res) => {
+  return res.sendFile("static/ekswagger-tarot-api-1.3-resolved.json", { root });
+});
+
+router.use((_req, res, next) => {
+  res.locals.rawData = JSON.parse(
+    fs.readFileSync("static/card_data.json", "utf8")
+  );
+  return next();
+});
+
+router.use(cors());  
+
+router.get("/", (_req, res) => {
+  return res.redirect("/api/v1/cards");
+});
+
+router.get("/cards", (_req, res) => {
+  const { cards } = res.locals.rawData;
+  return res.json({ nhits: cards.length, cards }).status(200);
+});
+
+router.get("/cards/search", (req, res) => {
+  const { cards } = res.locals.rawData;
+  console.log(`req.query:`, req.query);
+  if (!req.query || Object.keys(req.query).length === 0)
+    return res.redirect("/api/v1/cards");
+  let filteredCards = [];
+  for (let k in req.query) {
+    if (k !== "q") {
+      if (k === "meaning") {
+        filteredCards = cards.filter((c) =>
+          [c.meaning_up, c.meaning_rev]
+            .join()
+            .toLowerCase()
+            .includes(req.query[k].toLowerCase())
+        );
+      } else {
+        filteredCards = cards.filter(
+          (c) => c[k] && c[k].toLowerCase().includes(req.query[k].toLowerCase())
+        );
+      }
+    } else if (k === "q") {
+      filteredCards = cards.filter((c) =>
+        Object.values(c)
+          .join()
+          .toLowerCase()
+          .includes(req.query[k].toLowerCase())
+      );
+    }
+  }
+  return res
+    .json({ nhits: filteredCards.length, cards: filteredCards })
+    .status(200);
+});
+
+router.get("/cards/random", (req, res) => {
+  const { cards } = res.locals.rawData;
+  const n = req.query.n > 0 && req.query.n < 79 ? req.query.n : 78;
+  let cardPool = cloneDeep(cards);
+  let returnCards = [];
+  for (let i = 0; i < n; i++) {
+    let id = Math.floor(Math.random() * (78 - i));
+    let card = cardPool[id];
+    returnCards.push(card);
+    remove(cardPool, (c) => c.name_short === card.name_short);
+  }
+  return res.json({ nhits: returnCards.length, cards: returnCards });
+});
+
+router.get("/cards/:id", (req, res, next) => {
+  const { cards } = res.locals.rawData;
+  const card = cards.find((c) => c.name_short === req.params.id);
+  if (typeof card === "undefined") return next();
+  return res.json({ nhits: 1, card }).status(200);
+});
+
+router.get("/cards/suits/:suit", (req, res, next) => {
+  const { cards } = res.locals.rawData;
+  const cardsOfSuit = cards.filter((c) => c.suit === req.params.suit);
+  if (!cardsOfSuit.length) return next();
+  return res
+    .json({ nhits: cardsOfSuit.length, cards: cardsOfSuit })
+    .status(200);
+});
+
+router.get("/cards/courts", (_req, res) => {
+  const { cards } = res.locals.rawData;
+  const courtCards = cards.filter((c) =>
+    ["queen", "king", "page", "knight"].includes(c.value)
+  );
+  return res.json({ nhits: courtCards.length, cards: courtCards }).status(200);
+});
+
+router.get("/cards/courts/:court", (req, res, next) => {
+  const { cards } = res.locals.rawData;
+  const { court } = req.params;
+  const len = court.length;
+  if (len < 4) return next();
+  const courtSg = court[len - 1] === "s" ? court.slice(0, len - 1) : court;
+  const cardsOfCourt = cards.filter((c) => c.value === courtSg);
+  if (!cardsOfCourt.length) return next();
+  return res
+    .json({ nhits: cardsOfCourt.length, cards: cardsOfCourt })
+    .status(200);
+});
+
+router.use((_req, _res, next) => {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+router.use((err, _req, res) => {
+  res.status(err.status || 500);
+  res.json({ error: { status: err.status, message: err.message } });
+});
+
+const port = process.env.PORT || 9000;
+
+app.listen(port, () => {
+  console.log("RWS API Server now running on port", port);
+});
---
+++ api-atrot/scraper/sacred_texts_scraper.py
@@ -0,0 +1,120 @@
+# For reference only. Not using anymore.
+# A first effort at scraping with Python.
+# Use at your own risk. :)
+
+import json
+import re
+import requests
+from bs4 import BeautifulSoup
+
+base_url = 'http://www.sacred-texts.com/tarot/pkt/pkt'
+majors_url = 'http://www.sacred-texts.com/tarot/pkt/pkt0303.htm'
+
+cards = []
+minorText = []
+majorText = []
+
+class Card:
+    def __init__(self, value, value_int, name, name_short, meaning_up, meaning_rev):
+        self.value = value.lower()
+        self.value_int = value_int
+        self.name = name.title()
+        self.name_short = name_short.lower()
+        self.meaning_up = meaning_up
+        self.meaning_rev = meaning_rev
+
+class Major(Card):
+    def __init__(self, *args):
+        super(Major, self).__init__(*args)
+        self.type = "major"
+
+    def to_JSON(self):
+        return {
+            'name': self.name,
+            'name_short': self.name_short,
+            'value': self.value,
+            'value_int': self.value_int,
+            'meaning_up': self.meaning_up,
+            'meaning_rev': self.meaning_rev,
+            'type': self.type
+        }
+
+class Minor(Card):
+    def __init__(self, suit, desc, *args):
+        super(Minor, self).__init__(*args)
+        self.type = "minor"
+        self.name = self.value.capitalize() + ' of ' + suit.capitalize()
+        self.desc = desc
+        self.suit = suit.lower()
+
+    def to_JSON(self):
+        return {
+            'value': self.value,
+            'value_int': self.value_int,
+            'name': self.name,
+            'name_short': self.name_short,
+            'suit': self.suit,
+            'meaning_up': self.meaning_up,
+            'meaning_rev': self.meaning_rev,
+            'type': self.type,
+            'desc': self.desc
+        }
+
+def get_majors():
+    majs = requests.get(majors_url)
+    soup = BeautifulSoup(majs.content, 'html.parser')
+    for p in soup.find_all('p'):
+        line = p.text
+        m = re.match(r'([0-9]+|(ZERO))(\..+?(?=\.))', line)
+        if m:
+            value = m[1]
+            value_int = 0 if value == 'ZERO' else int(value)
+            name = m[3][2:]
+            name_short = 'ar' + '{:02}'.format(value_int)
+            meaning_up = line[len(m[0])+3:line.find("Reversed")]
+            meaning_rev = line[line.find("Reversed")+len("Reversed"):]
+            c = Major(value, value_int, name, name_short, meaning_up, meaning_rev)
+            entry = {'name_short': name_short, 'name': name, 'text': line, 'value': value}
+            majorText.append(entry)
+            cards.append(c.to_JSON())
+            print('Added major card', c.name)
+
+def get_minors():
+    suits_tup = [["wa", "wands"], ["cu", "cups"], ["pe", "pentacles"], ["sw", "swords"]]
+    mins_tup = [["pa", "page", 11], ["kn", "knight", 12], ["qu", "queen", 13], ["ki", "king", 14], ["ac", "ace", 1], ["02", "Two", 2], ["03", "Three", 3], ["04", "Four", 4], ["05", "Five", 5], ["06", "Six", 6], ["07", "Seven", 7], ["08", "Eight", 8], ["09", "Nine", 9], ["10", "Ten", 10]]
+
+    for suit in suits_tup:
+        for value in mins_tup:
+            page_url = base_url + suit[0] + value[0] + ".htm"
+            card_page = requests.get(page_url)
+            soup = BeautifulSoup(card_page.content, 'html.parser')
+            res = soup.select_one("p:nth-of-type(3)")
+            if(res):
+                value_long = value[1]
+                value_int = value[2]
+                suit_long = suit[1]
+                name_short = suit[0] + value[0]
+                name_long = value_long + ' of ' + suit_long
+                line = res.text
+                entry = {'name_short': name_short, 'text': line, 'value_long': value_long, 'value_int': value_int, 'name': name_long}
+                minorText.append(entry)
+                desc = line[:line.find("Divinatory Meanings")]
+                meaning_up = line[line.find("Divinatory Meanings")+len("Divinatory Meanings"):line.find("Reversed")]
+                meaning_rev = line[line.find("Reversed")+len("Reversed"):]
+                c = Minor(suit_long, desc, value_long, value_int, name_long, name_short, meaning_up, meaning_rev)
+                cards.append(c.to_JSON())
+                print('Added minor card ', c.name)
+
+get_majors()
+get_minors()
+
+
+with open('card_data_tmp.json', mode='w', encoding='utf-8') as f:
+    entry = {'count': len(cards), 'cards': cards}
+    json.dump(entry, f)
+
+with open('min_text.json', mode='w', encoding='utf-8') as f:
+    json.dump(minorText, f)
+
+with open('maj_text.json', mode='w', encoding='utf-8') as f:
+    json.dump(majorText, f)
