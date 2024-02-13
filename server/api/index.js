c003874566c5fe597b7011c6c2e82f67bec42cc0
PhurinGZ <120617446+PhurinGZ@users.noreply.github.com>
2024-02-13 16:49:49 +0700

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -24,6 +24,14 @@ app.use(bodyParser.urlencoded({ extended: false })); // Parse URL-encoded bodies
 // Database connection
 connection();
 
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization")
+    next()
+  });
+
 // Default route
 app.get("/", auth, (req, res) => {
   res.json({ data: req.user });
