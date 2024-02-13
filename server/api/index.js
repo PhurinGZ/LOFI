b8b404b5d6f425a0b3f7e8e02f23ced8f25916bf
PhurinGZ <120617446+PhurinGZ@users.noreply.github.com>
2024-02-13 17:11:54 +0700

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
 const connection = require("../db");
 const userRoutes = require("../routes/user");
 const authRoutes = require("../routes/auth");
@@ -12,15 +11,36 @@ const editor = require("../routes/editor");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 
-dotenv.config();
+// Function to allow CORS
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
 
+// Function to handle the response with the current date
+const handler = (req, res) => {
+  const d = new Date();
+  res.end(d.toString());
+};
+
+// Create an Express app
 const app = express();
 
 // Middleware Ordering:
 // Ensure correct middleware ordering
-const allowedOrigins = process.env.CORS_ALLOWED_ORIGINS.split(",");
-app.use(cors({ credentials: true, origin: allowedOrigins })); // Allow requests from specified origins
-app.options("*", cors()); // Handle preflight requests
+app.use(cors()); // Allow requests from all origins
 app.use(express.json()); // Parse JSON bodies
 app.use(cookieParser()); // Parse cookies
 app.use(bodyParser.urlencoded({ extended: false })); // Parse URL-encoded bodies
@@ -40,6 +60,9 @@ app.use("/api/login", authRoutes);
 app.use("/api/songs", songsRoutes);
 app.use("/api/playlists", playlistRoutes);
 app.use("/api/", searchRoutes);
+
+// Apply CORS middleware globally
+app.use(allowCors(handler));
 
 // Start the server
 const port = process.env.PORT || 8000;
