69117085c1a72d5fd4268635f57ceba6a3a0385c
PhurinGZ <120617446+PhurinGZ@users.noreply.github.com>
2024-02-15 05:50:19 +0700

    update vercel v0.1.2

--- tarot-api/app.js
+++ tarot-api/app.js
@@ -23,6 +23,7 @@ app.get("/", (_req, res) => {
 });
 
 app.use("/api/v1/", router);
+app.use('/api/v1/', cors());
 
 router.get("/docs", (_req, res) => {
   return res.sendFile("static/ekswagger-tarot-api-1.3-resolved.json", { root });
