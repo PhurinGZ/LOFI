f9f300fbb9215ccbd5604ab71360a4d2978d6039
PhurinGZ <120617446+PhurinGZ@users.noreply.github.com>
2024-02-15 06:20:19 +0700

    update vercelv0.1.6

--- tarot-api/app.js
+++ tarot-api/app.js
@@ -35,16 +35,16 @@ router.use((_req, res, next) => {
   return next();
 });
 
-router.use((_req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-  );
-  return next();
-});
+// router.use((_req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+//   );
+//   return next();
+// });
 
 // router.use(cors());
 
